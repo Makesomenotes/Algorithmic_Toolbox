57f5b5f8b9d7290e0d3124a3b755e704246fcc92
Maksim <gritskih.maksim@yandex.ru>
2021-07-29 17:55:50 +0300

    Add fibonachi task first

--- Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
+++ Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
@@ -13,7 +13,16 @@ def fibonacci_number_naive(n):
 def fibonacci_number(n):
     assert 0 <= n <= 45
 
-    type here
+    Fib_lst = []
+    for i in range(n+1):
+        if i <= 1:
+            Fib_lst.append(i)
+        else:
+            curr = Fib_lst[i-2] + Fib_lst[i-1]
+            Fib_lst.append(curr)
+        # print(Fib_lst[i])
+
+    return Fib_lst[n]
 
 
 if __name__ == '__main__':
